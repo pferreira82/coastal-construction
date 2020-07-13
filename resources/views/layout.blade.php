4511541073f1c1333db077aca64422d555310945
Peter <peter.e.ferreira@gmail.com>
2020-07-13 19:40:44 -0400

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>CoastalConstructionRI</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+		<meta name="description" content=""/>
+		<meta name="keywords" content=""/>
+		<link rel="stylesheet" href="assets/css/main.css"/>
+    </head>
+    <!-- Header -->
+    <header id="header">
+        <a class="logo" href="index.html">Welcome to Coastal Contruction & Millwork</a>
+        <nav>
+            <a href="#menu">Menu</a>
+        </nav>
+    </header>
+
+    <!-- Nav -->
+    <nav id="menu">
+        <ul class="links">
+            <li><a href="/">Home</a></li>
+            <li><a href="elements.html">About Us</a></li>
+            <li><a href="generic.html">Contact Us</a></li>
+        </ul>
+    </nav>
+    <body>
+        @yield('content')
+	</body>
+</html>
